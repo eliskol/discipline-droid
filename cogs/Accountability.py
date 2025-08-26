fc49fcfdd37324fbd2416b24cd859b013522376a
eliskol <75465281+eliskol@users.noreply.github.com>
2025-08-25 19:59:18 -0700

    saving open invitations works now

--- cogs/Accountability.py
+++ cogs/Accountability.py
@@ -20,12 +20,18 @@ client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 class Accountability(commands.Cog):
     def __init__(self, client):
         self.client = client
-        with open("cog/open_accountability_invitations.json", "r") as f:
-            self.open_invitations = json.load(f)
+        self.open_invitations = {}
+        with open("cogs/open_accountability_invitations.json", "r") as f:
+            weird_dict = json.load(f)
+            for key in weird_dict: # json stores keys as strings no matter what, when our datatype for the keys is really int.
+                self.open_invitations[int(key)] = weird_dict[key]
+        print(f"just loaded open invitations, we have {self.open_invitations}")
 
-    @tasks.loop(hours=1)
+    # @tasks.loop(hours=1)
     def save_open_invitations(self):
-        pass
+        with open("cogs/open_accountability_invitations.json", "w") as f:
+            json.dump(self.open_invitations, f)
+        print(f"just saved open invitations, we have {self.open_invitations}")
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -35,13 +41,7 @@ class Accountability(commands.Cog):
     async def accountability(self, ctx, *args):
         if args[0].lower() == "start" and len(args) > 1:
             print(args)
-            invited_member_str = args[1]
-            invited_member_obj = ctx.guild.get_member_named(args[1])
-            if invited_member_obj is None:
-                # check if they mentioned the person
-                if invited_member_str.startswith("<@") and invited_member_str.endswith(">"):
-                    invited_member_id = invited_member_str[2:-1]
-                    invited_member_obj = await ctx.guild.fetch_member(invited_member_id)
+            invited_member_obj = await get_member_obj_from_arg(ctx, args[1])
 
             if invited_member_obj is None:
                 await ctx.send(f"{ctx.author}, please try your command again.")
@@ -51,16 +51,30 @@ class Accountability(commands.Cog):
 
             # if invited_member_obj == ctx.author:
                 # await ctx.send(f"{ctx.author}, you cannot invite yourself to an Accountability Partnership!")
+                # return
 
-            if ctx.author.id in self.open_invitations and self.open_invitations[ctx.author.id] == invited_member_id:
+            if int(ctx.author.id) in self.open_invitations and self.open_invitations[int(ctx.author.id)] == invited_member_id:
                 await ctx.send(f"{ctx.author}, you have already invited {invited_member_obj} to an Accountability Partnership.")
-            elif invited_member_id in self.open_invitations and self.open_invitations[invited_member_id] == ctx.author.id:
+            elif invited_member_id in self.open_invitations and self.open_invitations[int(invited_member_id)] == int(ctx.author.id):
                 # members have succesfully invited each other to Accountability Partnership
                 self.start_accountability_partnership(invited_member_id, ctx.author.id)
             else:
                 await ctx.send(f"{ctx.author} has invited {invited_member_obj} to participate in an Accountability Partnership!")
-                self.open_invitations[ctx.author.id] = invited_member_obj.id
+                self.open_invitations[int(ctx.author.id)] = invited_member_obj.id
+                self.save_open_invitations()
                 await invited_member_obj.send(f"{invited_member_obj}, {ctx.author} has invited you to an Accountability Partnership!\nType `!accountability start {ctx.author}` in the accountability channel to accept their invitation!\nType `!accountability decline {ctx.author}` if you choose to decline.")
+
+        elif args[0].lower() == "decline" and len(args) > 1:
+            original_member = await get_member_obj_from_arg(ctx, args[1])
+            if original_member is None:
+                ctx.send("Please try your command again.")
+            if ctx.author.id not in self.open_invitations.values() or (ctx.author.id in self.open_invitations.values() and self.open_invitations[original_member.id] != ctx.author.id):
+                await ctx.send("Sorry, you do not have an accountability invitation from this person.")
+            elif self.open_invitations[original_member.id] == ctx.author.id:
+                del self.open_invitations[original_member.id]
+                await ctx.send(f"{ctx.author}, you have declined the accountability invitation from {original_member}.")
+                self.save_open_invitations()
+
         else:
             await ctx.send("You forgot to input arguments!")
 
@@ -69,3 +83,12 @@ class Accountability(commands.Cog):
 
 async def setup(client):
     await client.add_cog(Accountability(client))
+
+async def get_member_obj_from_arg(ctx, arg):
+    invited_member_obj = ctx.guild.get_member_named(arg)
+    if invited_member_obj is None:
+        # check if they mentioned the person
+        if arg.startswith("<@") and arg.endswith(">"):
+            invited_member_id = arg[2:-1]
+            invited_member_obj = await ctx.guild.fetch_member(invited_member_id)
+    return invited_member_obj
